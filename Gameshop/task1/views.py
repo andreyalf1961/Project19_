47484794aa68ece4c1e3f528c5d4fefe766e3c52
Andrey <Bazulin.a.i@gmail.com>
2024-12-09 16:59:53 +0300

    t

---
+++ Gameshop/task1/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer, Game
+
+
+# Create your views here.
+def platform(request):
+    pagename = 'Главная страница'
+    context = {'pagename': pagename}
+    return render(request, 'platform.html', context)
+
+
+def shop(request):
+    pagename = 'Игры'
+    games = Game.objects.all()
+    context = {'pagename': pagename, 'games': games}
+    return render(request, 'games.html', context)
+
+
+def cart(request):
+    pagename = 'Корзина'
+    context = {'pagename': pagename}
+    return render(request, 'cart.html', context)
+
+
+# Create your views here.
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+
+            if Buyer.objects.filter(name=username).exists():
+                info['error'] = 'Пользователь уже существует'
+                return render(request, 'registration_page.html', {'error': info['error']})
+            elif password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+                return render(request, 'registration_page.html', {'error': info['error']})
+            elif int(age) < 18:
+                info['error'] = 'Вы должны быть старше 18'
+                return render(request, 'registration_page.html', {'error': info['error']})
+            else:
+                info['welcome'] = f'Приветствуем, {username}!'
+                Buyer.objects.create(name=username, balance=10000, age=age)
+                return render(request, 'registration_page.html', {'welcome': info['welcome']})
+
+    else:
+        form = UserRegister()
+    return render(request, 'registration_page.html', {'form': form})
+
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if Buyer.objects.filter(name=username).exists():
+            info['error'] = 'Пользователь уже существует'
+            return render(request, 'registration_page.html', {'error': info['error']})
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+            return render(request, 'registration_page.html', {'error': info['error']})
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+            return render(request, 'registration_page.html', {'error': info['error']})
+        else:
+            info['welcome'] = f'Приветствуем, {username}!'
+            Buyer.objects.create(name=username, balance=10000, age=age)
+            return render(request, 'registration_page.html', {'welcome': info['welcome']})
+
+
+
+    return render(request, 'registration_page.html')
+from django.shortcuts import render
+
+# Create your views here.
