dbf087f8e456b5e89c1564dab6cd4efeeb823067
Andrey <Bazulin.a.i@gmail.com>
2024-12-10 08:53:48 +0300

    adm

--- Gameshop/task1/admin.py
+++ Gameshop/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('size', 'cost')
+    list_display = ('title','cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age')
+    list_display=('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields =('balance',)
+
+
+
